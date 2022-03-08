135e04a3113802a4e655f39eb7924efe87fde04a
terez50 <nagy.peterne.terez@gmail.com>
2022-03-08 10:56:27 +0100

    Paginator works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { User } from './model/user';
 import { ConfigService, ITableCol } from './service/config.service';
 import { UserService } from './service/user.service';
+
+interface IPageBtn {
+  page: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -13,12 +17,35 @@ import { UserService } from './service/user.service';
     title = 'str-angular-http-client';
     // userList$: Observable<User[]> = this.userService.getAll();
     userList$: Observable<User[]> = this.userService.list$.pipe(
-      map( users => users.filter( user => user.id > 20))
+      map( users => users.filter( user => user.id > 0)),
+      tap( users => this.productsProperties.count = users.length )
     );
     cols: ITableCol[] = this.config.tableCols;
     phrase: string = '';
     filterKey: string = 'last_name';
     filterKeys: string[] = Object.keys(new User());
+
+    // Paginator
+    productsProperties: {count: number} = {count: 0};  // a termékek száma (hány van összesen)
+    pageSize: number = 10;    // egyszerre megjelenítendő termékek (egy oldalon) (46:15)
+    pageStart: number = 1;    // kezdőoldal (jelenleg mi van az oldallista elején a gombokon)
+    currentPage: number = 1;  // ahol a kurzor áll (az a gomb, amihez tartozó adatokat látom)
+    get paginator(): IPageBtn[] {  // IPageBtn: interfész; visszaad egy tömböt
+      const pages: IPageBtn[] = [];
+      for (let i = 0; i < this.productsProperties.count / this.pageSize && pages.length < 10; i++) {  // legeneráljuk a gombokat (pages)
+        const page = this.pageStart + i; // hogy ne nullával kezdődjön a gombok szövege
+        pages.push({page});
+      }
+      return pages;
+    }
+    get pageSliceStart(): number {  // a megjelenített oldalak vágását melyik terméknél kezdjem el
+      const index = this.currentPage -1;  // merthogy a 0-ás 1-esként jelenik meg a gombon
+      // return index === 0 ? 0 : (index * this.pageSize);
+      return index * this.pageSize;
+    }
+    get pageSliceEnd(): number {  // meddig vágjon
+      return this.pageSliceStart + this.pageSize;
+    }
 
     constructor(
       private userService: UserService,
@@ -37,6 +64,19 @@ import { UserService } from './service/user.service';
 
     onDelete(user: User): void {
       this.userService.remove(user);
+    }
+
+    onPaginate(ev: Event, btn: IPageBtn): void {
+      ev.preventDefault();          // hogy ne frissüljön az oldal
+      this.currentPage = btn.page;
+      this.pageStart = (btn.page - 5) < 1 ? 1 : (btn.page - 5);
+      // console.log(btn); (28:10)
+    }
+
+    onStepPage(ev: Event, step: number): void {
+      ev.preventDefault();
+      this.currentPage += step;
+      this.pageStart += step;
     }
 
 }
--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { User } from '../model/user';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any[] | null, key: string, phrase: string | number | boolean): any[] | null {
+  transform(
+    value: User[] | null,
+    key: string,
+    phrase: string | number | boolean,
+    props: {count: number}): User[] | null {
     if (!Array.isArray(value) || !key || !phrase) {
       // console.log('null');
       return value;
@@ -13,7 +18,7 @@ export class FilterPipe implements PipeTransform {
 
     phrase = typeof phrase === 'number' ? phrase : ('' + phrase).toLowerCase();
 
-    return value.filter(item => {
+    const filtered = value.filter(item => {
 
       if (typeof item[key] !== 'number') {
         // console.log('not number', ('' + item[key]).toLowerCase(), phrase, typeof item[key], typeof phrase, ('' + item[key]).toLowerCase().includes(phrase as string));
@@ -23,6 +28,12 @@ export class FilterPipe implements PipeTransform {
       return ('' + item[key]) === phrase;
 
     });
+
+    if (props?.count) {
+      props.count = filtered.length; // itt állítom be a szűrt hosszat
+    }
+
+    return filtered;
   }
 
 }
